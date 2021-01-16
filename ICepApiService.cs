d5a0e3207d3984bc81ef62c5487ff531778397c7
asilva90 <49291782+asilva90@users.noreply.github.com>
2021-01-15 21:26:34 -0300

    Criação das classes de requisição e retorno da solicitação.

---
+++ ICepApiService.cs
@@ -0,0 +1,11 @@
+﻿using Refit;
+using System.Threading.Tasks;
+
+namespace ApiTest
+{
+    public interface ICepApiService
+    {
+        [Get("/ws/{cep}/json")]
+        Task<CepResponse> GetAdressAsync(string cep);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
+using System.Net;
+using System.IO;
+using System.Threading.Tasks;
+using Refit;
 
-namespace ApiCep
+namespace ApiTest
 {
-    class Program
+    public class MainClass
     {
-        static void Main(string[] args)
+
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            static string Consulta()
+            {
+                Console.WriteLine("Informe seu Cep: ");
+
+                string cepInformado = Console.ReadLine().ToString();
+                Console.WriteLine("Consultando Informações do Cep: " + cepInformado);
+
+                return cepInformado;
+            }
+
+            try
+            {
+                var cepClient = RestService.For<ICepApiService>("http://viacep.com.br");
+                var cepInformado = Consulta();
+                while (cepInformado != null)
+                {
+                    var adress = await cepClient.GetAdressAsync(cepInformado);
+
+                    Console.WriteLine($"\nCep: {adress.Cep}, \nComplemento: {adress.Complemento}, \nLogradouro: {adress.Logradouro}, \nBairro: {adress.Bairro}, \nCidade: {adress.Localidade}, \nUF: {adress.Uf}, \nUnidade: {adress.Unidade}, \n");
+                    Console.ReadKey();
+
+                    cepInformado = Consulta();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erro ao buscar CEP: " + e.Message);
+            }
         }
     }
 }
